6b1ff4dfc42e4c792151109830ca2c21a97eff20
guoyufu-study <guoyufu_study@163.com>
2022-08-16 13:50:29 +0800

    19.7 使用接口组织枚举：将枚举实例向上转型

---
+++ src/main/java/cn/jasper/java5/enums/menu/TypeOfFood.java
@@ -0,0 +1,12 @@
+package cn.jasper.java5.enums.menu;
+
+public class TypeOfFood {
+
+    public static void main(String[] args) {
+        Food salad = Food.Appetizer.SALAD;
+        Food lasagne = Food.MainCourse.LASAGNE;
+        Food gelato = Food.Dessert.GELATO;
+        Food cappuccino = Food.Coffee.CAPPUCCINO;
+
+    }
+}
